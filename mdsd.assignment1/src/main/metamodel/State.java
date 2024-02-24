aad5968357319ad8e62b258e7e48c639ab1fb73a
Sebastian <sechr17@student.sdu.dk>
2024-02-24 11:34:31 +0100

    Uploaded finished DSL

---
+++ mdsd.assignment1/src/main/metamodel/State.java
@@ -0,0 +1,37 @@
+package main.metamodel;
+
+import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class State {
+	private String name;
+	private List<Transition> transitions = new ArrayList<>();
+	
+	public State(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void addTransition(Transition transition) {
+		transitions.add(transition);
+	}
+
+	public List<Transition> getTransitions() {
+		return transitions;
+	}
+
+	public Transition getTransitionByEvent(String event) {
+		for (Transition transition : transitions) {
+	        if (transition.getEvent().equals(event)) {
+	            return transition;
+	        }
+	    }
+	    return null;
+	}
+}
